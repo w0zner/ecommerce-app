e5fa949b5a9cd926874cc79506f1fae2f9b96dd4
Cesar Ramirez <cesar_ramirez@hacienda.gov.py>
2026-06-18 16:55:39 -0300

    ajuste

--- resources/views/livewire/admin/products/product-variants.blade.php
+++ resources/views/livewire/admin/products/product-variants.blade.php
@@ -1,3 +1,14 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <section class="rounded-lg bg-white p-4 shadow-md">
+        <header>
+            <div>
+                <h1>
+                    Opciones
+                </h1>
+                <x-button custom wire:click="openModalForSave()" class="bg-[#2b3440] text-white text-sm px-4 py-2 hover:bg-[#1d232a]">
+                    <i class="fa-solid fa-plus"></i> Agregar
+                </x-button>
+            </div>
+        </header>
+    </section>
 </div>
