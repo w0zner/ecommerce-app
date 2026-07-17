1bfdcfee01304b716c6fa4a436d8eeb007550d8d
Cesar Ramirez <cesar_ramirez@hacienda.gov.py>
2026-07-17 16:32:22 -0300

    Disenho del header pagina publica

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,8 @@
         <x-banner />
 
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @livewire('navigation-menu')
+            {{-- @livewire('navigation-menu') --}}
+            @livewire('navigation')
 
             <!-- Page Heading -->
             @if (isset($header))
---
+++ resources/views/livewire/navigation.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <header class="bg-purple-600">
+        <x-container class="px-4">
+            <h1 class="text-white">
+                <a href="">
+                    <span class="text-3xl font-semibold">Ecommerce</span>
+                    <span class="text-xs">Tienda Online</span>
+                </a>
+            </h1>
+        </x-container>
+    </header>
+</div>
