82a1f379f4a95a464840cb246dfd97287f947014
Cesar Ramirez <cesar_ramirez@hacienda.gov.py>
2026-07-20 16:33:08 -0300

    Ajuste navigation page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
+        <script src="https://kit.fontawesome.com/c711b8278b.js" crossorigin="anonymous"></script>
+
         <!-- Styles -->
         @livewireStyles
     </head>
--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,12 +1,33 @@
 <div>
     <header class="bg-purple-600">
-        <x-container class="px-4">
-            <h1 class="text-white">
-                <a href="">
-                    <span class="text-3xl font-semibold">Ecommerce</span>
-                    <span class="text-xs">Tienda Online</span>
-                </a>
-            </h1>
-        </x-container>
+            <x-container class="px-4 py-4">
+                <div class="flex justify-between items-center space-x-8">
+                    <button class="text-2xl btn btn-ghost">
+                        <i class="fas fa-bars text-white"></i>
+                    </button>
+                    <h1 class="text-white">
+                        <a href="/" class="inline-flex flex-col items-end">
+                            <span class="text-3xl leading-4 md:leading-6 font-semibold">Ecommerce</span>
+                            <span class="text-xs">Tienda Online</span>
+                        </a>
+                    </h1>
+                    <div class="flex-1 hidden md:block">
+                        <input type="text" placeholder="Buscar producto" class="input input-sm w-full" />
+                    </div>
+
+                    <div class="flex items-center space-x-4 md:space-x-8">
+                        <button class="text-2xl btn btn-ghost">
+                            <i class="fas fa-user text-white"></i>
+                        </button>
+                        <button class="text-2xl btn btn-ghost">
+                            <i class="fas fa-shopping-cart text-white"></i>
+                        </button>
+                    </div>
+                </div>
+
+                <div class="mt-4 md:hidden">
+                    <input type="text" placeholder="Buscar producto" class="input input-sm w-full" />
+                </div>
+            </x-container>
     </header>
 </div>
