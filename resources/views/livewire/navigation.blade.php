62fc7b76d1acd283fa4d880b09f4ed3c3abe1026
Cesar Ramirez <cesar_ramirez@hacienda.gov.py>
2026-07-21 16:29:20 -0300

    add sidebar lateral

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -30,4 +30,39 @@
                 </div>
             </x-container>
     </header>
+
+    <div class="fixed top-0 left-0 inset-0 bg-black bg-opacity-25 z-10">
+    </div>
+
+    <div class="fixed top-0 left-0 z-20">
+        <div class="flex">
+            <div class="w-80 h-screen bg-white">
+                <div class="bg-purple-600 px-4 py-3 text-white font-semibold">
+                    <div class="flex justify-between items-center">
+                        <span class="text-lg">
+                        Hola
+                    </span>
+                    <button>
+                        <i class="fas fa-times"></i>
+                    </button>
+                    </div>
+                </div>
+                <div class="h-[calc(100vh-52px)] overflow-auto">
+                    <ul>
+                        @foreach ($families as $family)
+                            <li>
+                                <a href="" class="flex items-center justify-between px-4 py-4 text-gray-700 hover:bg-purple-200">
+                                    {{ $family->name }}
+                                    <i class="fa-solid fa-angle-right"></i>
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
 </div>
